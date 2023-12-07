9bc13d41bf8c3d004e8f17ff4282c02f48262f82
Maurice Ampt <maurice.ampt@amplexor.com>
2023-12-07 22:40:58 +0100

    solution for day 5 part 2

--- 5/src/index.ts
+++ 5/src/index.ts
@@ -1,30 +1,50 @@
 import * as fs from 'fs';
 
+const part2 = true;
+
 let input = fs.readFileSync('./resources/input.txt', 'utf-8').split(/\r?\n/);
 
 let seeds = input.splice(0, 1)[0].split(':')[1].trim().split(' ').map(x => +x);
 
-// while
+function* SeedGenerator() {
+    if (!part2) {
+        for (let i = 0; i < seeds.length; i++) {
+            yield seeds[i];
+        }
+    }
+    else {
+        for (let i = 0; i <= seeds.length; i += 2) {
+            let seed = seeds[i];
+            console.log('processing range of seeds');
+            for (let j = 0; j < seeds[i+1]; j++) {
+                if (j % 10000000 === 0) console.log('10.000.000 seeds processed')
+                yield seed + j;
+            }
+        }
+    }
+}
+
 let lut: { sourceStart: number, destinationStart: number, length: number }[][] = [];
 for (const line of input) {
     if (line.trim() === '') {
         continue
     }
     if (line.endsWith('map:')) {
-        console.log(`starting new layer: ${line}`);
+        // console.log(`starting new layer: ${line}`);
         lut.push([]);
         continue;
     }
     const [destinationStart, sourceStart, length] = line.split(' ').map(x => +x);
     lut[lut.length - 1].push({sourceStart, destinationStart, length});
 }
-const result = seeds.reduce((acc, seed) => {
+
+function lookupLocationForSeed(seed: number) {
     let currentLookup = seed;
     // iterate over all layers to end up with the location
     for (const element of lut) {
         //iterate over lookup tables to find match, if none is found, keep currentLookup identical
         for (const l of element) {
-            if (currentLookup >= l.sourceStart && currentLookup <= l.sourceStart + l.length - 1 ) {// 50 + 5 - 1 - 52 = 2,
+            if (currentLookup >= l.sourceStart && currentLookup <= l.sourceStart + l.length - 1) {// 50 + 5 - 1 - 52 = 2,
                 // console.log(`found match in lut, looking up ${currentLookup}, lut: ${l.sourceStart}, ${l.destinationStart}, ${l.length}, destination: ${l.destinationStart + currentLookup + l.sourceStart}`)
                 currentLookup = l.destinationStart + currentLookup - l.sourceStart;
                 break;
@@ -32,6 +52,14 @@ const result = seeds.reduce((acc, seed) => {
         }
     }
     // console.log(`Seed ${seed} ended up at location ${currentLookup}`);
-    return acc === -1 ? currentLookup : Math.min(currentLookup, acc);
-}, -1);
-console.log(result);
+    return currentLookup;
+}
+
+let min;
+let seedGenerator = SeedGenerator();
+for (let seed of seedGenerator) {
+    let locationForSeed = lookupLocationForSeed(seed);
+    if (min && locationForSeed < min) console.log(`new min: ${locationForSeed}`)
+    min = min === undefined ? locationForSeed : Math.min(locationForSeed, min);
+}
+console.log(min);
